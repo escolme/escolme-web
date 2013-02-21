739e610af758e39cb9fd748c0348b5be21f97e4e
Jose Luis Orozco <handark@gmail.com>
2013-02-21 11:10:33 -0500

    Carga Inicia

---
+++ js/main.js
@@ -0,0 +1,13 @@
+<form>
+    <fieldset>
+        <legend>Inscripcion en Linea - Datos Personales</legend>
+        <label>Label</label>
+        <select ng-model="ASPIRANTE.ASPI_TIPODOCUMENTO"  >
+            <option> -- Seleccione el Tipo de Documento</option>
+        </select>
+
+
+
+        <button type="submit" class="btn btn-primary" >Submit</button>
+    </fieldset>
+</form>
