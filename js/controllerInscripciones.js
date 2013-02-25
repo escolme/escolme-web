927cff23891beef50f101113c2d4c5a9e800497d
Jose Luis Orozco <handark@gmail.com>
2013-02-25 09:18:27 -0500

    Creacion de la clase servicios.js

--- js/controllerInscripciones.js
+++ js/controllerInscripciones.js
@@ -1,3 +1,12 @@
-function InscripcionCtrl($scope,$http){
+function InscripcionCtrl(uiService,sessionService,$scope,$http){
+
+    uiService.mostrarMenuSuperior(false);
+
+    function Limpiar() {
+        $scope.inscripcion = {
+            id_usuario:0,login:'',sexo:'M',nombre:'',idDependencia:'',email:'',telefono:'',jefe:'',usuarioactivo:'S',
+            passwordnew:'',id_perfil_grupo:0,id_empresa:sessionStorage.getItem("stIdEmpresa"),es_cad:false
+        };
+    }
 
 }
