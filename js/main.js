49beee83672c169ada8d4d7de96ef3a317e561c5
Jose Luis Orozco <sistemas@escolme.edu.co>
2013-03-18 17:50:15 -0500

    Listar modalidad

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var httpheaders = "application/json; charset=utf-8";
 
 var escolmeWeb = angular.module('escolmeWeb',[]).config(['$routeProvider', function ($routeProvider,$locationProvider) {
     $routeProvider.
-        when('/', { templateUrl: 'vistas/index.html', controller: IndexCtrl }).
+        when('/', { templateUrl: 'vistas/inscripcion.html', controller: IndexCtrl }).
         when('/inscripcion', { templateUrl: 'vistas/inscripcion.html', controller: InscripcionCtrl }).
         otherwise({ redirectTo: '/' });
 }]);
