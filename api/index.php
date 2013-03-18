f7f84054d7e1bb084e19d19ae254bf025f86e6e9
Jose Luis Orozco <sistemas@escolme.edu.co>
2013-03-18 08:55:05 -0500

    Listar modalidad

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@ require 'modelo/modalidad.php';
 
 $app = new Slim();
 
-$app->get('/listar', 'modalidadListar');
+$app->get('/modalidad/listar', 'modalidadListar');
 
 
 $app->get('/prueba/:name', function ($name) {
