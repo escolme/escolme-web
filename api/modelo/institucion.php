2ec1e168f5f750875043623d89541cce17012c4b
escolme <informatica@informatica.(none)>
2013-04-02 10:50:47 -0500

    instituciones

--- api/modelo/institucion.php
+++ api/modelo/institucion.php
@@ -28,3 +28,31 @@ function institucionListar(){
         echo '{"error: ":' . $e->getMessage() . '}';
     }
 }
+
+function institucionListarPorFiltro($filtro){
+    try{
+        $resultados = array();
+        $conexion = new conexionBD();
+        $conn = $conexion->conectar();
+        $sql = "SELECT ACADEMICO.INSTITUCION.INST_CODIGOSNP, ACADEMICO.INSTITUCION.INST_NOMBREINSTITUCION, ACADEMICO.IEM.INST_CODIGOSNP, ACADEMICO.INSTITUCION.TIIN_ID, ACADEMICO.INSTITUCION.INST_JORNADA, ACADEMICO.INSTITUCION.MUNI_IDINSTITUCION FROM ACADEMICO.IEM INNER JOIN ACADEMICO.INSTITUCION ON ACADEMICO.IEM.INST_CODIGOSNP = ACADEMICO.INSTITUCION.INST_CODIGOSNP WHERE (((ACADEMICO.INSTITUCION.TIIN_ID)='1')) AND ACADEMICO.INSTITUCION.INST_NOMBREINSTITUCION LIKE '%".strtoupper($filtro)."%' ORDER BY ACADEMICO.INSTITUCION.INST_NOMBREINSTITUCION";
+        $query =OCIParse($conn, $sql);
+        OCIExecute($query, OCI_DEFAULT);
+        while (OCIFetch($query))
+        {
+            $fila = array(
+                "INST_CODIGOSNP" => ociresult($query, "INST_CODIGOSNP"),
+                "INST_NOMBREINSTITUCION" => ociresult($query, "INST_NOMBREINSTITUCION"),
+                "INST_JORNADA" => ociresult($query, "INST_JORNADA")
+            );
+            array_push($resultados, $fila);
+        }
+
+        echo utf8_encode('{"datos": ' . json_encode($resultados) . '}');
+
+        OCILogoff($conn);
+    }
+    catch(Exception $e){
+        OCILogoff($conn);
+        echo '{"error: ":' . $e->getMessage() . '}';
+    }
+}
