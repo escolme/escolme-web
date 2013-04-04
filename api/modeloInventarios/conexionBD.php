1adb01391834981b5b40241690666e7b4d56728a
Samuel Guevara <sistemas@escolme.edu.co>
2013-04-04 11:21:46 -0500

    ´bd´

---
+++ api/modeloInventarios/conexionBD.php
@@ -0,0 +1,6 @@
+<?php
+
+$conexionBD = mysql_connect("localhost", "root","") or die(mysql_error());
+$db = mysql_select_db("inventarios") or die(mysql_error());
+
+?>
---
+++ api/modeloInventarios/productos.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: samuel1189
+ * Date: 4/04/13
+ * Time: 10:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
