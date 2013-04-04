1bd1e0dbb16656f470cfec616ef9ec40ba61dfc7
Julian David Sanchez <analista@escolme.edu.co>
2013-04-04 18:03:52 -0500

    ver mas - valida prog

--- js/controllerInscripciones.js
+++ js/controllerInscripciones.js
@@ -136,10 +136,24 @@ function InscripcionCtrl(sessionService,$scope,$http){
         });
     }
 
-    $scope.InformacionAdicional = function($event){
-    $scope.infoadional= response.data.datos;
-    $('#ventanaInformacionAdicional').modal('show');
-    $event.preventDefault();
+    $scope.InformacionAdicional = function(){
+        $('#ventanaInformacionAdicional').modal('show');
+    }
+
+    $scope.VerPensum =function(){
+        $('#frmPensum').attr('src','recursos/pensum/' + $scope.inscripcion.PROG_ID + '.pdf');
+    }
+
+    $scope.VerPensum2 =function(){
+        $('#frmPensum').attr('src','recursos/pensum/' + $scope.inscripcion.PROG_ID2 + '.pdf');
+    }
+
+    $scope.ValidarPrograma =function(){
+        if ($scope.inscripcion.PROG_ID == $scope.inscripcion.PROG_ID2){
+            alert('[ERROR] No puede elegir el mismo programa');
+            $scope.inscripcion.PROG_ID2=null ;
+        }
+
 
     }
 
